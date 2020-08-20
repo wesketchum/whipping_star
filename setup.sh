390d48e703a44258eb55f2c84324e1efe3ea9df6
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2020-08-20 15:38:23 -0500

    modifed SL7 setpu.sh for sbnfit

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@ source /grid/fermiapp/products/uboone/setup_uboone.sh;
 setup git v2_15_1; 
 setup gitflow v1_11_0; 
 setup mrb v1_16_02;
-setup root v6_10_04d -q e14:nu:prof; 
+#setup root v6_10_04d -q e14:nu:prof; #Old SL6  
+setup root v6_12_04e -q e15:prof; 
 setup cmake v3_11_4;
 setup eigen v3_3_4a
 
@@ -13,7 +14,4 @@ export SBNFITDIR=$me
 export SBNFIT_LIBDIR=$me/build
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SBNFIT_LIBDIR
-
-
-
 
